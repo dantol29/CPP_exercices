f42b17e27bebf5dcfee7b60afc3b8e08ea9b47c1
dantol29 <dantol29@gmail.com>
2024-02-29 16:01:16 +0100

    ex04 done, only tests left

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -1,16 +1,71 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <string.h>
+
+std::string getString(char **argv, std::ifstream& file)
+{
+	std::string	text;
+	std::string	fileText;
+	int			index;
+	int			skip;
+
+	while (std::getline(file, text))
+		fileText.append(text);
+	if (fileText.find(argv[2]) == std::string::npos)
+		return ("");
+	skip = 0;
+	while (1){
+		index = fileText.find(argv[2], skip);
+		if (index == std::string::npos)
+			break ;
+		else{
+			int	i = index;
+			int	j = 0;
+			while (fileText[i] && fileText[i] == argv[2][j++])
+				i++;
+			fileText.erase(index, i - index);
+			fileText.insert(index, argv[3], strlen(argv[3]));
+			skip = index + strlen(argv[3]);
+		}
+	}
+	return (fileText);
+}
+
+void replaceFile(std::string filename, std::ifstream& file, char **argv)
+{
+	std::string	fileText;
+
+	fileText = getString(argv, file);
+	if (fileText.empty()){
+		std::cout << "No matches found!" << std::endl;
+		return ;
+	}
+	filename = filename + ".replace";
+	std::ofstream outputFile(filename.c_str());
+	if (!outputFile.is_open())
+	{
+		std::cout << "Could not create a file!" << std::endl;
+		return ;
+	}
+	outputFile << fileText;
+}
 
 int main(int argc, char **argv)
 {
-	std::string text;
+	std::string filename;
+
 	if (argc != 4)
 	{
 		std::cout << "Only 3 arguments allowed" << std::endl;
 		return (1);
 	}
-	std::ifstream file(argv[1]);
-	while (std::getline(file, text))
-		std::cout << text << std::endl;
+	filename = argv[1];
+	std::ifstream file(filename.c_str());
+	if (!file.is_open())
+	{
+		std::cout << "File does not exist!" << std::endl;
+		return (1);
+	}
+	replaceFile(filename, file, argv);
 }
